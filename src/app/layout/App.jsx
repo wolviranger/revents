4d73cd15b51ea7f7fd9518d8c8dca27e373109e5
tacketmt <mtackett11@gmail.com>
2018-11-29 11:54:44 -0600

    grids added

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
